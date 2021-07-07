87f8501978b29db32348b85a20c1d9e2156d6f77
Livia <livinha.guimaraes.013@gmail.com>
2021-07-07 16:43:53 -0300

    add initial setup

---
+++ mangapi/src/main/java/one/digitalinnovation/mangapi/MangapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.mangapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangapiApplication.class, args);
+	}
+
+}
---
+++ mangapi/src/test/java/one/digitalinnovation/mangapi/MangapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.mangapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
