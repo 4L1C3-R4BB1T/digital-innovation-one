46a65d1d9921bf01e6a5205ff6a9d43e70341ce8
Livia <livinha.guimaraes.013@gmail.com>
2021-07-07 20:07:36 -0300

    update swagger configuration

--- personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -30,32 +30,27 @@ public class PersonController implements PersonControllerOpenApi {
 
     private PersonService personService;
 
-    @Override
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return personService.createPerson(personDTO);
     }
 
-    @Override
     @GetMapping
     public List<PersonDTO> listAll() {
         return personService.listAll();
     }
 
-    @Override
     @GetMapping("/{id}")
     public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
         return personService.findById(id);
     }
 
-    @Override
     @PutMapping("/{id}")
     public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
         return personService.updateById(id, personDTO);
     }
 
-    @Override
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
--- personapi/src/main/java/one/digitalinnovation/personapi/swagger/config/SwaggerConfig.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/swagger/config/SwaggerConfig.java
@@ -1,119 +1,41 @@
 package one.digitalinnovation.personapi.swagger.config;
 
-import java.util.Arrays;
-import java.util.List;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.fasterxml.classmate.TypeResolver;
-
-import one.digitalinnovation.personapi.exception.config.Problem;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@Import(BeanValidatorPluginsConfiguration.class)
-public class SwaggerConfig implements WebMvcConfigurer {
-	
-	TypeResolver typeResolver = new TypeResolver();
-	
-	@Bean
-	public Docket apiDocket() {		
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-					.apis(RequestHandlerSelectors.basePackage("one.digitalinnovation.personapi"))
-					.paths(PathSelectors.any())
-					.build()
-				.useDefaultResponseMessages(false)
-				.globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
-				.globalResponseMessage(RequestMethod.POST, globalPostPutResponseMessages())
-				.globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
-				.globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
-				.additionalModels(typeResolver.resolve(Problem.class))
-				.apiInfo(apiInfo());
-	}
-	
-	private List<ResponseMessage> globalGetResponseMessages() {
-		return Arrays.asList(
-				new ResponseMessageBuilder()
-					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-					.message("Internal server error")
-					.responseModel(new ModelRef("Problem"))
-					.build(),
-				new ResponseMessageBuilder()
-					.code(HttpStatus.NOT_ACCEPTABLE.value())
-					.message("Resource has no representation that could be accepted by the consumer")
-					.build());
-	}
-	
-	private List<ResponseMessage> globalPostPutResponseMessages() {
-		return Arrays.asList(
-				new ResponseMessageBuilder()
-					.code(HttpStatus.BAD_REQUEST.value())
-					.message("Invalid request (customer error)")
-					.responseModel(new ModelRef("Problem"))
-					.build(),
-				new ResponseMessageBuilder()
-					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-					.message("Internal server error")
-					.responseModel(new ModelRef("Problem"))
-					.build(),
-				new ResponseMessageBuilder()
-					.code(HttpStatus.NOT_ACCEPTABLE.value())
-					.message("Resource has no representation that could be accepted by the consumer")
-					.build(),
-				new ResponseMessageBuilder()
-					.code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
-					.message("Request refused because the body is in an unsupported format")
-					.responseModel(new ModelRef("Problem"))
-					.build());
-	}
-	
-	private List<ResponseMessage> globalDeleteResponseMessages() {
-		return Arrays.asList(
-				new ResponseMessageBuilder()
-					.code(HttpStatus.BAD_REQUEST.value())
-					.message("Invalid request")
-					.responseModel(new ModelRef("Problem"))
-					.build(),
-				new ResponseMessageBuilder()
-					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-					.message("Internal server error")
-					.responseModel(new ModelRef("Problem"))
-					.build());
-	}
-	
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("Person API")
-				.description("Person API Project")
-				.version("1.0")
-				.build();
-	}
-	
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html")
-			.addResourceLocations("classpath:/META-INF/resources/");
-		
-		registry.addResourceHandler("/webjars/**")
-			.addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}
-	
+public class SwaggerConfig {
+
+	private static final String BASE_PACKAGE = "one.digitalinnovation.personapi.controller";
+    private static final String API_TITLE = "Person API";
+    private static final String API_DESCRIPTION = "Person API Project";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version("1.0")
+                .build();
+    }
+    
 }
